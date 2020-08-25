4223a71b2e8a54b19595973895ce20f94e485d1f
Mauro Coletes <mauro.coletes@gmail.com>
2020-08-24 22:19:19 -0300

    add initial project

---
+++ TheCatsLibrary/TheCatsLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheCatsLibrary
+//
+//  Created by Mauro Coletes on 24/08/20.
+//  Copyright © 2020 Mauro Coletes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
