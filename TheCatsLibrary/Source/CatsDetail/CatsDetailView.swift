4e941e4c0cb0ad5ea02c2ba1e4ff66efdcc6f252
Mauro Coletes <mauro.coletes@gmail.com>
2020-08-29 13:01:01 -0300

    start cat detail

---
+++ TheCatsLibrary/Source/CatsDetail/CatsDetailView.swift
@@ -0,0 +1,9 @@
+//
+//  CatsDetailView.swift
+//  TheCatsLibrary
+//
+//  Created by Mauro Coletes on 29/08/20.
+//  Copyright © 2020 Mauro Coletes. All rights reserved.
+//
+
+import Foundation
---
+++ TheCatsLibrary/Source/CatsDetail/CatsDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CatsDetailViewModel.swift
+//  TheCatsLibrary
+//
+//  Created by Mauro Coletes on 29/08/20.
+//  Copyright © 2020 Mauro Coletes. All rights reserved.
+//
+
+import Foundation
