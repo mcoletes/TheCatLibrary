5d07a631de55387ce812165bf383b4ffe39aca96
Mauro Coletes <mauro.coletes@gmail.com>
2020-09-02 19:03:46 -0300

    update data source, delegate, prefetch to a different class

--- TheCatsLibrary/Source/Features/CatsList/CatsListViewController.swift
+++ TheCatsLibrary/Source/Features/CatsList/CatsListViewController.swift
@@ -36,6 +36,7 @@ class CatsListViewController: UIViewController, CatsListDisplayLogic, ViewContro
     
     private var viewModel = CatsList.ViewModel()
     private var footerHeight: CGFloat = 80
+    private var catsListDelegates: CatsListCollectionViewDelegates?
     
     // MARK: - Init
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -71,6 +72,8 @@ class CatsListViewController: UIViewController, CatsListDisplayLogic, ViewContro
         presenter.viewController = viewController
         router.viewController = viewController
         router.dataStore = interactor
+        
+        catsListDelegates = CatsListCollectionViewDelegates(cats: [], interactor: interactor)
     }
     
     
@@ -86,15 +89,15 @@ class CatsListViewController: UIViewController, CatsListDisplayLogic, ViewContro
     // MARK: - Internal Methods
     
     func startLoading() {
-        footerHeight = 50
+        catsListDelegates?.footerHeight = 50
     }
     
     func stopLoading() {
-        footerHeight = 0
+        catsListDelegates?.footerHeight = 0
     }
     
     func showCats(cats: [CatsList.CatVM]) {
-        viewModel.cats.append(contentsOf: cats)
+        catsListDelegates?.cats.append(contentsOf: cats)
         
         DispatchQueue.main.async {
             self.collectionView.reloadData()
@@ -107,9 +110,10 @@ class CatsListViewController: UIViewController, CatsListDisplayLogic, ViewContro
     // MARK: - Private Methods
     
     private func setupCollectionView() {
-        collectionView.prefetchDataSource = self
-        collectionView.dataSource = self
-        collectionView.delegate = self
+        collectionView.prefetchDataSource = catsListDelegates
+        collectionView.dataSource = catsListDelegates
+        collectionView.delegate = catsListDelegates
+        
         self.collectionView.register(LoadingView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: LoadingView.defaultReuseIdentifier)
         self.collectionView.register(CatsListCell.self)
     }
@@ -120,66 +124,5 @@ class CatsListViewController: UIViewController, CatsListDisplayLogic, ViewContro
     
     private func fetchCats() {
         interactor?.fetchListOfCats()
-    }
-}
-
-extension CatsListViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    
-    // MARK: - UICollectionViewDataSource, UICollectionViewDelegate
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.cats.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: CatsListCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
-        let cat = viewModel.cats[indexPath.row]
-        cell.setup(name: cat.name, description: cat.description, color: cat.color.color)
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        interactor?.didSelectCat(for: indexPath.row)
-    }
-}
-extension CatsListViewController: UICollectionViewDataSourcePrefetching {
-    
-    // MARK: - UICollectionViewDataSourcePrefetching
-    
-    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
-        for indexPath in indexPaths {
-            interactor?.loadNextPageIfNeeded(for: indexPath.row)
-        }
-    }
-}
-
-extension CatsListViewController: UICollectionViewDelegateFlowLayout {
-    
-    // MARK: - UICollectionViewDelegateFlowLayout
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cat = viewModel.cats[indexPath.row]
-        let width = collectionView.bounds.width - (2 * CatsListCell.kStackViewBorders)
-        
-        let titleSize = cat.name.height(withConstrainedWidth: width , font: CatsListCell.kFontTitle)
-        let descriptionSize = cat.description.height(withConstrainedWidth: width, font: CatsListCell.kFontDescription)
-        
-        //title necessary height + description necessaryHeight + Cats List stack view spacing between elements + 2 times borders (top and bottom)
-        //used this aproach because prefetch does not work when colection view layout is set to UICollectionViewFlowLayout.automaticSize
-        return CGSize(width: collectionView.bounds.width, height: (titleSize + descriptionSize + CatsListCell.kSpacingStackViewElements + (2 * CatsListCell.kStackViewBorders)))
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: footerHeight)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        
-        switch kind {
-        case UICollectionView.elementKindSectionFooter:
-            return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: LoadingView.defaultReuseIdentifier, for: indexPath)
-        default:
-            assert(false, "Unexpected element kind")
-        }
     }
 }
---
+++ TheCatsLibrary/Source/Features/CatsList/CollectionView/CatsListCollectionViewDelegates.swift
@@ -0,0 +1,79 @@
+//
+//  CatsListCollectionViewDelegates.swift
+//  TheCatsLibrary
+//
+//  Created by Mauro Coletes on 02/09/20.
+//  Copyright © 2020 Mauro Coletes. All rights reserved.
+//
+
+import UIKit
+
+class CatsListCollectionViewDelegates: NSObject, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var cats: [CatsList.CatVM]
+    var footerHeight: CGFloat = 50
+    
+    private var interactor: CatsListBusinessLogic?
+    
+    init(cats: [CatsList.CatVM] = [], interactor: CatsListBusinessLogic) {
+        self.interactor = interactor
+        self.cats = cats
+    }
+
+    // MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cats.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: CatsListCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+        let cat = cats[indexPath.row]
+        cell.setup(name: cat.name, description: cat.description, color: cat.color.color)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        interactor?.didSelectCat(for: indexPath.row)
+    }
+}
+
+extension CatsListCollectionViewDelegates: UICollectionViewDelegateFlowLayout {
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cat = cats[indexPath.row]
+        let width = collectionView.bounds.width - (2 * CatsListCell.kStackViewBorders)
+        
+        let titleSize = cat.name.height(withConstrainedWidth: width , font: CatsListCell.kFontTitle)
+        let descriptionSize = cat.description.height(withConstrainedWidth: width, font: CatsListCell.kFontDescription)
+        
+        //title necessary height + description necessaryHeight + Cats List stack view spacing between elements + 2 times borders (top and bottom)
+        //used this aproach because prefetch does not work when colection view layout is set to UICollectionViewFlowLayout.automaticSize
+        return CGSize(width: collectionView.bounds.width, height: (titleSize + descriptionSize + CatsListCell.kSpacingStackViewElements + (2 * CatsListCell.kStackViewBorders)))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: footerHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        switch kind {
+        case UICollectionView.elementKindSectionFooter:
+            return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: LoadingView.defaultReuseIdentifier, for: indexPath)
+        default:
+            assert(false, "Unexpected element kind")
+        }
+    }
+}
+
+extension CatsListCollectionViewDelegates: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            interactor?.loadNextPageIfNeeded(for: indexPath.row)
+        }
+    }
+}
+
